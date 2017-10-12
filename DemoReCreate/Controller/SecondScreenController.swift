8e56c97a55e37cea4ab87abef26d64536c0236b7
Aaron Montoya <aatoya7@gmail.com>
2017-10-12 13:11:07 -0600

    Added another screen to the story board and a controller for it.

---
+++ DemoReCreate/Controller/SecondScreenController.swift
@@ -0,0 +1,14 @@
+//
+//  SecondScreenController.swift
+//  DemoReCreate
+//
+//  Created by Montoya, Aaron on 10/12/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class SecondScreenController : UIViewController
+{
+    
+}
