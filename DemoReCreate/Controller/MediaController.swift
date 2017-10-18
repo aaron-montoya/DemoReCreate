9194b2a4dfac12708079c9bd5ac08c286c0cf34c
Aaron Montoya <aatoya7@gmail.com>
2017-10-18 12:45:14 -0600

    Added ChangeImage method.

--- DemoReCreate/Controller/MediaController.swift
+++ DemoReCreate/Controller/MediaController.swift
@@ -10,10 +10,15 @@ import UIKit
 
 public class MediaController: UIViewController
 {
+    @IBOutlet weak var UIImageView: UIImageView!
+    @IBOutlet weak var LeftButton: UIButton!
+    @IBOutlet weak var RightButton: UIButton!
+    @IBOutlet weak var FirstSlider: UISlider!
     
     
     private lazy var colour : ColourTools = ColourTools()
-    private var imageCounter : Int = 0
+    private var image_counter : Int = 0
+    
     
     public override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +31,32 @@ public class MediaController: UIViewController
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func ChangeImage(_ sender: UIButton)
+    {
+        changeImage()
+        view.backgroundColor = colour.createRandomColour()
+    }
     
-    
+    private func changeImage() -> Void
+    {
+        if image_counter > 2
+        {
+        image_counter = 0;
+        }
+        
+        if image_counter == 0
+        {
+            UIImageView.image = UIImage(named: "RedPanda")
+        }
+        else if image_counter == 1
+        {
+            UIImageView.image = UIImage(named: "Minion")
+        }
+        else
+        {
+            UIImageView.image = UIImage(named: "Cody")
+        }
+        
+        image_counter += 1
+    }
 }
